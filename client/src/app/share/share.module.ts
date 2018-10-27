5ffb10b75faa23b26e0bcc12681269e58dfb1e0d
Ins1eme <insieme1337@gmail.com>
2018-10-27 13:18:38 +0300

    fixed routes

--- client/src/app/share/share.module.ts
+++ client/src/app/share/share.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FilmComponent } from './components/film/film.component'
+import { FilmComponent } from './components/film/film.component';
+import { TabsComponent } from './components/tabs/tabs.component'
 
 @NgModule({
-  declarations: [FilmComponent],
+  declarations: [FilmComponent, TabsComponent],
   imports: [
     CommonModule,
     BrowserAnimationsModule
--- routes/films.js
+++ routes/films.js
@@ -3,7 +3,6 @@ const controller = require('../controllers/films');
 const router = express.Router();
 
 router.get('/films', controller.getAllFilms);
-router.post('/films', controller.addFilm);
 
 
 module.exports = router;
